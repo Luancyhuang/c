5c70e28e8d63a83548fc65e76bd422cad8e79c6e
Luancyhuang <542914825@qq.com>
2021-09-11 17:27:02 +0800

    字符金字塔

--- test2.c
+++ test2.c
@@ -1,28 +1,28 @@
-//char *strtok(char *str, const char *delim);
-//delim 参数是个字符串，定义了用作分隔符的字符集合。
-//strtok 
 #include <stdio.h>
-#include <string.h>
+//字符金字塔
 int main()
 {
-  //192.168.12.12 点分十进制的表示方式IP地址的
-  //192 168 12 12
-  char arr[] = "542914825@qq.com";
-  const char *p = "@.";
-  //strtok(arr,p);
-  char buf[1024] = {0};
-  strcpy(buf,arr);
-  //切割buf 中的字符串
-  //char *ret = strtok(arr,p);
-  //printf("%s\n",ret);
-  //ret = strtok(NULL,p);
-  //printf("%s\n",ret);
-  //ret = strtok(NULL,p);
-  //printf("%s\n",ret);
-  char *ret = NULL;
-  for(ret=strtok(arr,p);ret != NULL;ret=strtok(NULL,p))
+  //输入一个字符
+  char ch = 0;
+  ch = getchar();
+  //输出金字塔
+  int i = 0;
+  //每循环一次，打印一行
+  //每一行由两部分组成，一是空格，二是字符
+  for (i=0;i<5;i++)
   {
-    printf("%s\n",ret);
+    //空格
+    int j = 0;
+    for(j=0;j<4-i;j++)
+    {
+      printf(" ");
+    }
+    //字符
+    for(j=0;j<=i;j++)
+    {
+      printf("%c ",ch);
+    }
+    printf("\n");
   }
   return 0;
 }
