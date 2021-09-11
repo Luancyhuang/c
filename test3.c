4decf2a5e43f60982346d588d67eef71bde50a41
Luancyhuang <542914825@qq.com>
2021-09-11 21:05:33 +0800

    数字转ascii码

--- test3.c
+++ test3.c
@@ -1,32 +1,16 @@
-//实现一个函数，可以左旋字符串中的k个字符。
-//例如：ABCD左旋一个字符得到BCDA
-//ABCD左旋两个字符得到CDAB
 #include <stdio.h>
-#include <string.h>
-#include <assert.h>
-//typedef unsiged int u_int;//类型重命名
-void left_move(char *arr,int k)
-{
- assert(arr);
- int i = 0;
- int len = strlen(arr);
- for(i=0;i<k;i++)
- {
-   //左旋转一个字符
-   char tmp = *arr;//1.把第一个元素放到一个临时变量（tmp)存储。
-   int j = 0;
-   for(j=0;j<len-1;j++)
-   {
-     *(arr+j) = *(arr+j+1);
-   }//2.依次把后面的元素挪移到前面一个位置。
-   *(arr+len-1) = tmp;//3.把移动的元素移动到最后一个位置。
- }
-}
+//求数字对应的ascii码
 int main()
-{ 
- // u_int x = 0;  
-  char arr[] = "abcdef";
-  left_move(arr,2);
-  printf("%s\n",arr);
+{
+  //arr 是数组，数组是用下标来访问，下标是从0开始的。
+  char arr[] = {73,32,99,97,110,32,100,111,32,105,116,33};
+  int i = 0;
+  //求数组元素个数
+  int sz = sizeof(arr)/sizeof(arr[0]);
+  for(i=0;i<sz;i++)
+  {
+    printf("%c ",arr[i]);
+  }
+  printf("\n");
   return 0;
 }
