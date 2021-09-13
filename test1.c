2fef9e6fc5c7018679946868fabfe148df1b3979
Luancyhuang <542914825@qq.com>
2021-09-13 19:22:39 +0800

    3科成绩求和和平均值

--- test1.c
+++ test1.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+
 int main()
 {
-  int *p = (int*)malloc(5*sizeof(int));
-  if(p == NULL)
-  { 
-    //打印错误信息的方式
-    printf("%s\n",strerror(errno));
-  }
-  else 
+  double score[3] = {0};
+  //输入
+  int i = 0;
+  double sum = 0.0;
+  for(i=0;i<3;i++)
   {
-    //正常使用空间
-    int i = 0;
-    for(i = 0; i < 5;i++)
-    {
-      *(p+i) = i;
-    }
-    for(i = 0; i < 5;i++)
-    {
-      printf("%d ",*(p+i));
-    }
+    scanf("%lf",&score[i]);
+    sum += score[i];
   }
+  //输出
+  printf("%.2lf %.2lf\n",sum,sum/3);
   return 0;
 }
