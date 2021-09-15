4306c6afd8dd9ce22021ee007e87a4b33b99f190
Luancyhuang <542914825@qq.com>
2021-09-15 15:06:33 +0800

    判断三角形

--- test.c
+++ test.c
@@ -1,27 +1,65 @@
 #include <stdio.h>
-//假设你们社团竞选社长，有两名候选人分别是A和B,社团每名同学必须并且只能投一票，最终得票最多的人为社长。
+//根据输入输出相应的内容
+//int main()
+//{
+//  int n = 0;
+//  while(~scanf("%d",&n))
+//  {
+//    if(n>0)
+//    {
+//      printf("%d\n",1);
+//    }
+//    else while(n<0)
+//    {
+//      printf("%.1f\n",0.5);
+//    }
+//    else
+//    {
+//      printf("%d\n",0);
+//    }
+//  }
+//  return 0;
+//}
+//三角形的判断
 int main()
 {
-  char buf[100] = {0};
-  //输入
-  gets(buf);
-  //统计
-  int flag = 0;
-  int i = 0;
-  while(buf[i]!='0')
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  while(~scanf("%d%d%d",&a,&b,&c))
   {
-    if(buf[i]=='A')
-      flag++;
-    if(buf[i]=='B')
-      flag--; 
-    i++;
+    //1.判断是否为三角形
+    if((a+b>c)&&(a+c>b)&&(b+c>a)) 
+    {
+      if((a==b) && (b==c))
+      {
+        printf("等边三角形\n");
+      }
+      else if((a==b&& a!=c)||(a==c&&a!=b)||(b==c && b!=a))
+      {
+        printf("等腰三角形\n");
+      }
+      else
+      {
+        printf("一般三角形\n");
+      }
+    }
+    else//不是三角形
+    {
+      printf("不是三角形\n");
+    }
   }
-  if(flag>0)
-    printf("A\n");
-  else if(flag<0)
-    printf("B\n");
-  else
-    printf("E\n");
-  //输出
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
