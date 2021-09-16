2019bc58c364a196998767e1b50ed65504dab690
Luancyhuang <542914825@qq.com>
2021-09-16 22:46:04 +0800

    倒置三角形打印

--- test1.c
+++ test1.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-
+//带空格的前置三角形
 int main()
 {
   int n = 0;
-  while(scanf("%d",&n)!=EOF)
+  while(~scanf("%d",&n))
   {
     int i = 0;
     for(i=0;i<n;i++)
     {
-      printf("*");
-    }
-    printf("\n");
+      int j = 0;
+      for(j=0;j<n-1-i;j++)
+      {
+        printf("  ");      
+      }
+      for(j=0;j<=i;j++)
+      {
+        printf("* ");
+      }
+      printf("\n");
+    }  
   }
   return 0;
 }
