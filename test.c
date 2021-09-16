1fc3467de3bbba1303a1387244b494f4d48f8712
centos7 <weplayer@localhost.localdomain>
2021-09-15 23:23:33 -0700

    简单计算器

--- test.c
+++ test.c
@@ -1,65 +1,40 @@
 #include <stdio.h>
-//根据输入输出相应的内容
-//int main()
-//{
-//  int n = 0;
-//  while(~scanf("%d",&n))
-//  {
-//    if(n>0)
-//    {
-//      printf("%d\n",1);
-//    }
-//    else while(n<0)
-//    {
-//      printf("%.1f\n",0.5);
-//    }
-//    else
-//    {
-//      printf("%d\n",0);
-//    }
-//  }
-//  return 0;
-//}
-//三角形的判断
+//简单计算器
 int main()
 {
-  int a = 0;
-  int b = 0;
-  int c = 0;
-  while(~scanf("%d%d%d",&a,&b,&c))
+  double n1 = 0.0;
+  double n2 = 0.0;
+  char op = 0;
+  while(~scanf("%lf%c%lf",&n1,&op,&n2))
   {
-    //1.判断是否为三角形
-    if((a+b>c)&&(a+c>b)&&(b+c>a)) 
+    switch(op)
     {
-      if((a==b) && (b==c))
-      {
-        printf("等边三角形\n");
-      }
-      else if((a==b&& a!=c)||(a==c&&a!=b)||(b==c && b!=a))
-      {
-        printf("等腰三角形\n");
-      }
-      else
-      {
-        printf("一般三角形\n");
-      }
+      case '+':
+        printf("%.4lf+%.4lf=%.4lf\n",n1,n2,n1+n2);
+        break;
+      case '-':
+        printf("%.4lf-%.4lf=%.4lf\n",n1,n2,n1-n2);
+        break;
+      case '*':
+        printf("%.4lf*%.4lf=%.4lf\n",n1,n2,n1*n2);
+        break;
+      case '/':
+        if(n2==0.0)
+        {
+          printf("被除数不能为能'0'\n");
+        }
+        else
+        {	
+          printf("%.4lf/%.4lf=%.4lf\n",n1,n2,n1/n2);
+        }
+        break;
+      default:
+        printf("非法输入\n");
+        break;
     }
-    else//不是三角形
-    {
-      printf("不是三角形\n");
-    }
+  
+  
   }
+
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
