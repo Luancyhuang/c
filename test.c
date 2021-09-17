3577f4c2c2346e344a7a70e7acc6b17956a095e0
Luancyhuang <542914825@qq.com>
2021-09-17 15:51:01 +0800

    倒置金字塔

--- test.c
+++ test.c
@@ -1,43 +1,5 @@
 #include <stdio.h>
-//正方形
-//int main()
-//{
-//  int n = 0;
-//  int i = 0;
-//  while(~scanf("%d",&n))
-//  {
-//    for(i=0;i<n;i++)
-//    {
-//      int j = 0;
-//      for(j=0;j<n;j++)
-//      {
-//        printf("* ");
-//      }
-//      printf("\n");
-//    }
-//  }
-//  return 0;
-//}
-//三角形
-//int main()
-//{
-//  int n = 0;
-//  int i = 0;
-//  while(~scanf("%d",&n))
-//  {
-//    for(i=0;i<n;i++)
-//    {
-//      int j = 0;
-//      for(j=0;j<=i;j++)
-//      {
-//        printf("* ");
-//      }
-//      printf("\n");
-//    }  
-//  }
-//  return 0;
-//} 
-//倒置三角形
+//倒置金字塔
 int main()
 {
   int n = 0;
@@ -47,12 +9,16 @@ int main()
     for(i=0;i<n;i++)
     {
       int j = 0;
+      for(j=0;j<i;j++)
+      {
+        printf(" ");      
+      }
       for(j=0;j<n-i;j++)
       {
-        printf("* ");      
+        printf("* ");
       }
       printf("\n");
-    }  
+    }
   }
   return 0;
 }
