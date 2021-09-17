a55551f6d4c89a64637f76818891e5c4f0133b2c
Luancyhuang <542914825@qq.com>
2021-09-17 18:11:59 +0800

    k型图

--- test1.c
+++ test1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//带空格的前置三角形
+//k型图
 int main()
 {
   int n = 0;
@@ -9,16 +9,29 @@ int main()
     for(i=0;i<n;i++)
     {
       int j = 0;
-      for(j=0;j<n-1-i;j++)
+      for(j=0;j<n+1-i;j++)
       {
-        printf("  ");      
+        printf("* ");
       }
+      for(j=0;j<i;j++)
+      {
+        printf(" ");
+      }
+      printf("\n");
+    }
+    for(i=0;i<n+1;i++)
+    {
+      int j = 0;
       for(j=0;j<=i;j++)
       {
         printf("* ");
       }
+      for(j=0;j<n-i;j++)
+      {
+        printf(" ");
+      }
       printf("\n");
-    }  
+    }
   }
   return 0;
 }
