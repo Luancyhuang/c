b1ec65693df95da6603a7508c82df0a8b352a73b
Luancyhuang <542914825@qq.com>
2021-09-18 09:44:40 +0800

    反对角线图形打印

--- test.c
+++ test.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 //输入n行n列星形图
+//int main()
+//{
+//  int n = 0;
+//  while(~scanf("%d",&n))
+//  {
+//    int i = 0;
+//    for(i=0;i<n;i++)
+//    {
+//      int j = 0;
+//      for(j=0;j<n;j++)
+//      {
+//        if(i==j)
+//	{
+//	  printf("*");
+//	}
+//        else
+//	{
+//	  printf(" ");
+//	}
+//      }
+//      printf("\n");
+//    }
+//  }
+//  return 0;
+//}
+//反向输出对角线图形
 int main()
 {
   int n = 0;
-  while(~scanf("%d",&n))
+  while(scanf("%d",&n))
   {
     int i = 0;
     for(i=0;i<n;i++)
@@ -11,17 +37,17 @@ int main()
       int j = 0;
       for(j=0;j<n;j++)
       {
-        if(i==j)
+        if(i+j==n-1)
 	{
 	  printf("*");
 	}
-        else
+	else
 	{
 	  printf(" ");
 	}
       }
-      printf("\n");
-    }
+      printf("\n");    
+    }  
   }
   return 0;
 }
