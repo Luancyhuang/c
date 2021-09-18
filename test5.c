155b565b34905debfeec24359c405e2ff873ec02
Luancyhuang <542914825@qq.com>
2021-09-18 14:53:32 +0800

    数字三角形

--- test5.c
+++ test5.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
-
-//int main()
-//{
-//  int a = 0;
-//  int b = 0;
-//  //输入
-//  scanf("%d %d",&a,&b);
-//  //计算
-//  int sum = ((a+b)%100)%100;
-//  printf("%d\n",sum);
-//  return 0;
-//}
-
-
-//给定一个浮点数，要求得到该浮点数的各位数
+//数字三角形
 int main()
 {
-  double d = 0.0;
-  scanf("%lf",&d);
-  int n = (int) d;
-  printf("%d\n",n%10);
+  int n = 0;
+  while(~scanf("%d",&n))
+  {
+    int i = 0;
+    for(i=1;i<=n;i++)
+    {
+      int j = 0;
+      for(j=1;j<=i;j++)
+      {
+        printf("%d ",j);      
+      }
+      printf("\n");
+    }  
+  }
   return 0;
 }
-
-
-
-
-
-
 
