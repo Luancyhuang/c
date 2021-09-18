0100e0f05009d0a146df763292809b9fa4344b8a
Luancyhuang <542914825@qq.com>
2021-09-18 10:10:51 +0800

    空心正方形

--- test2.c
+++ test2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//箭型图
+//空心正方形
 int main()
 {
   int n = 0;
@@ -9,29 +9,23 @@ int main()
     for(i=0;i<n;i++)
     {
       int j = 0;
-      for(j=0;j<n-i;j++)
+      for(j=0;j<n;j++)
       {
-        printf("  ");
-      }
-      for(j=0;j<=i;j++)
-      {
-        printf("*");
-      }
-      printf("\n");
-    }
-    for(i=0;i<n+1;i++)
-    {
-      int j = 0;
-      for(j=0;j<i;j++)
-      {
-        printf("  ");
-      }
-      for(j=0;j<n+1-i;j++)
-      {
-        printf("*");
+        if(i==0||i==n-1)
+	{
+	  printf("* ");
+	}
+	else if(j==0||j==n-1)
+	{
+	  printf("* ");
+	}
+	else
+	{
+          printf("  ");
+	}
       }
       printf("\n");
     }
-  }
+  }  	
   return 0;
 }
