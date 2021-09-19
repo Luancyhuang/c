e9c0d415ef78fb3acef7d3707de4dce7aefec83e
Luancyhuang <542914825@qq.com>
2021-09-19 14:52:26 +0800

    求数值1～N中奇偶数的个数

--- test.c
+++ test.c
@@ -1,53 +1,18 @@
 #include <stdio.h>
-//输入n行n列星形图
-//int main()
-//{
-//  int n = 0;
-//  while(~scanf("%d",&n))
-//  {
-//    int i = 0;
-//    for(i=0;i<n;i++)
-//    {
-//      int j = 0;
-//      for(j=0;j<n;j++)
-//      {
-//        if(i==j)
-//	{
-//	  printf("*");
-//	}
-//        else
-//	{
-//	  printf(" ");
-//	}
-//      }
-//      printf("\n");
-//    }
-//  }
-//  return 0;
-//}
-//反向输出对角线图形
+//任意输入一个正整数N，统计1～N之间奇数的个数和偶数的个数，并输出。
 int main()
 {
   int n = 0;
-  while(scanf("%d",&n))
+  scanf("%d",&n);
+  int odd = 0;
+  int i = 0;
+  for(i=1;i<=n;i++)
   {
-    int i = 0;
-    for(i=0;i<n;i++)
+    if(i%2 == 1)
     {
-      int j = 0;
-      for(j=0;j<n;j++)
-      {
-        if(i+j==n-1)
-	{
-	  printf("*");
-	}
-	else
-	{
-	  printf(" ");
-	}
-      }
-      printf("\n");    
+      odd++;    
     }  
   }
+  printf("%d %d\n",odd,n-odd);
   return 0;
 }
