5b5af63a9fac5940c9499af61d1b097b4d1d5a27
Luancyhuang <542914825@qq.com>
2021-09-20 19:54:54 +0800

    删除数列中的指定数字

--- test1.c
+++ test1.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-//用户登录网站，通常需要注册，一半需要输入两遍密码，请编程判断输入的两次密码是否一致，一致输出"same"，不一致输出"different"。
+//数列中删除一个指定的数字
 int main()
 {
-  char password1[100] = {0};
-  char password2[100] = {0};
-  scanf("%s %s",&password1,&password2);
-  if(strcmp(password1,password2) == 0)
+  int n = 0;
+  int arr[50] = {0};
+  int del = 0;
+  scanf("%d",&n);
+  int i = 0;
+  for(i=0;i<n;i++)
   {
-    printf("same\n");  
+    scanf("%d",arr[i]);
   }
-  else
+  scanf("%d",&del);
+  int j = 0;
+  for(i=0;i<n;i++)
   {
-    printf("different\n");  
+    if(arr[i] != del)
+    {
+      arr[j++] = arr[i];
+    }
+  }
+  for(i=0;i<j;i++)
+  {
+    printf("%d",arr[i]);
   }
   return 0;
 }
