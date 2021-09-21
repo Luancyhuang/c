9ef39d9c1ebeea5b32c587f196441dba42bfc727
Luancyhuang <542914825@qq.com>
2021-09-21 17:58:28 +0800

    数列去重

--- test.c
+++ test.c
@@ -1,52 +1,37 @@
 #include <stdio.h>
-//输入n个成绩，换行输出n个成绩中最高分数和最低分数的差
-//int main()
-//{
-//  int n = 0;
-//  scanf("%d",&n);
-//  int i = 0;
-//  int score = 0;
-//  int max = 0;
-//  int min = 100;
-//  for(i=0;i<n;i++)
-//  {
-//    scanf("%d",&score);
-//    if(score>max)
-//    {
-//      max = score;
-//    }
-//    if(score<min)
-//    {
-//      min = score;
-//    }
-//  }
-//  printf("%d\n",max-min);  
-//  return 0;
-//}
-//
-//有序序列的判断
+//输入n个整数的序列，要求对这个序列进行去重操作。所谓去重，是指对这个序列中每个重复出现的整数，只保留该数第一次出现的位置，删除其余位置。
 int main()
 {
   int n = 0;
-  int arr[50] = {0};
+  int arr[1000] = {0};
   scanf("%d",&n);
-  int flag1 = 0;
-  int flag2 = 0;
   int i = 0;
   for(i=0;i<n;i++)
   {
-    scanf("%d",&arr[i]);
-    if(i>0)
+    scanf("%d",arr[i]);
+  } 
+  for(i=0;i<n;i++)
+  {
+    //判断下标为i的元素是否在后面出现
+    int j = 0;
+    for(j=i+1;j<n;j++)
     {
-      if(arr[i] > arr[i-1])
-        flag1 = 1;   
-      else
-        flag2 = 1;
-    }
+      if(arr[i] == arr[j])
+      {
+        //去重，后面的元素往前覆盖
+	int k = 0;
+	for(k=j;k<n-1;k++)
+	{
+	  arr[k] = arr[k+1];
+	}
+        n--;
+	j--;
+      }    
+    }    
   }
-  if(flag1+flag2==1)
-    printf("sorted\n");
-  else
-    printf("unsorted\n");
+  for(i=0;i<n;i++)
+  {
+    printf("%d ",arr[i]);  
+  }
   return 0;
 }
