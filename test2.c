de4cb0617aaa415f0603991024f01d0198f11521
Luancyhuang <542914825@qq.com>
2021-09-22 11:01:05 +0800

    判断是否是上三角矩阵

--- test2.c
+++ test2.c
@@ -48,45 +48,75 @@
 //  return 0;
 //}
 //矩阵相等判断
+//int main()
+//{
+//  int n = 0;
+//  int m = 0;
+//  scanf("%d %d",&n,&m);
+//  int arr1[10][10] = {0};
+//  int arr2[10][10] = {0};
+//  int i = 0;
+//  int j = 0;
+//  for(i=0;i<n;i++)
+//  {
+//    for(j=0;j<m;j++)
+//    {
+//      scanf("%d",&arr1[i][j]);    
+//    }  
+//  }
+//  for(i=0;i<n;i++)
+//  {
+//    for(j=0;j<m;j++)
+//    {
+//      scanf("%d",&arr2[i][j]);    
+//    }  
+//  } 
+//  for(i=0;i<n;i++)
+//  {
+//    for(j=0;j<m;j++)
+//    {
+//      if(arr1[i][j] != arr2[i][j])
+//      {
+//        printf("No!\n");
+//	return 0;      
+//      }
+//    }
+//  }
+//  printf("Yes\n");
+//  return 0;
+//}
+//上三角矩阵的判断
 int main()
 {
   int n = 0;
-  int m = 0;
-  scanf("%d %d",&n,&m);
-  int arr1[10][10] = {0};
-  int arr2[10][10] = {0};
+  scanf("%d",&n);
+  int arr[10][10] = {0};
   int i = 0;
   int j = 0;
   for(i=0;i<n;i++)
   {
-    for(j=0;j<m;j++)
+    for(j=0;j<n;j++)
     {
-      scanf("%d",&arr1[i][j]);    
+      scanf("%d",&arr[i][j]);    
     }  
   }
   for(i=0;i<n;i++)
   {
-    for(j=0;j<m;j++)
+    for(j=0;j<n;j++)
     {
-      scanf("%d",&arr2[i][j]);    
-    }  
-  } 
-  for(i=0;i<n;i++)
-  {
-    for(j=0;j<m;j++)
-    {
-      if(arr1[i][j] != arr2[i][j])
+      if(i>j)
       {
-        printf("No!\n");
-	return 0;      
+        if(arr[i][j]!=0)
+	{
+	  printf("No\n");
+	  return 0;	
+	}      
       }
     }
   }
-  printf("Yes\n");
+  printf("Yes\n");  
   return 0;
 }
-
-
 
 
 
