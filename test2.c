bc12ab29da177ea7c8ed064c59368e71533c963f
Luancyhuang <542914825@qq.com>
2021-09-22 09:33:45 +0800

    数组中求出最大元素的坐标

--- test2.c
+++ test2.c
@@ -1,31 +1,60 @@
 #include <stdio.h>
-//空心正方形
+
+//int main()
+//{
+//  int n = 0;
+//  int m = 0;
+//  scanf("%d %d",&n,&m);
+//  int i = 0;
+//  int j = 0;
+//  int num = 0;
+//  for(i=1;i<=n*m;i++)
+//  {
+//    scanf("%d",&num);
+//    printf("%d ",num);
+//    if(i%m == 0)
+//    {
+//      printf("\n");
+//    }
+//  }
+//  return 0;
+//}
+//从n行m列的方阵队列找到身高最高的人的位置，请你帮助他完成这个任务。
 int main()
 {
   int n = 0;
-  while(~scanf("%d",&n))
+  int m = 0;
+  scanf("%d %d",&n,&m);
+  int i = 0;
+  int j = 0;
+  int num = 0;
+  int max = 0;
+  int x = 0;
+  int y = 0;
+  for(i=0;i<n;i++)
   {
-    int i = 0;
-    for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
     {
-      int j = 0;
-      for(j=0;j<n;j++)
+      scanf("%d",&num);
+      if(num>max)
       {
-        if(i==0||i==n-1)
-	{
-	  printf("* ");
-	}
-	else if(j==0||j==n-1)
-	{
-	  printf("* ");
-	}
-	else
-	{
-          printf("  ");
-	}
+        max = num;
+	x = i;
+	y = j;
       }
-      printf("\n");
-    }
-  }  	
+    }  
+  }
+  printf("%d %d\n",x+1,y+1);
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
