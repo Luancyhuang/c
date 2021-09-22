1e5e8d20b5ce1059484f4d7e22465980cd9a263e
Luancyhuang <542914825@qq.com>
2021-09-22 18:16:42 +0800

    进制相互转化

--- test5.c
+++ test5.c
@@ -1,76 +1,21 @@
 #include <stdio.h>
-//数字三角形
-//int main()
-//{
-//  int n = 0;
-//  while(~scanf("%d",&n))
-//  {
-//    int i = 0;
-//    for(i=1;i<=n;i++)
-//    {
-//      int j = 0;
-//      for(j=1;j<=i;j++)
-//      {
-//        printf("%d ",j);      
-//      }
-//      printf("\n");
-//    }  
-//  }
-//  return 0;
-//}
-//公务员考试
-//int main()
-//{
-//  int score = 0;
-//  int i = 0;
-//  int sum = 0;
-//  int max = 0;
-//  int min = 100;
-//  for(i=0;i<7;i++)
-//  {
-//    scanf("%d",&score);
-//    sum += score;
-//    if(max<score)
-//      max = score;
-//    if(min<score)
-//      min = score;
-//  }
-//  printf("%.2f\n",(sum - max -min)/5.0);
-//  return 0;
-//}
-//在一个有序数列中插入一个数字
+
 int main()
 {
   int n = 0;
-  int arr[51] = {0};
-  int m = 0;
+  int arr[40] = {0};
   scanf("%d",&n);
   int i = 0;
-  for(i=0;i<n;i++)
+  while(n)
   {
-    scanf("%d",&arr[i]);  
+    arr[i] = n%6;
+    i++;
+    n/=6;
   }
-  scanf("%d",&m);
-  for(i=n-1;i>=0;i--)
+  for(i--;i>=0;i--)
   {
-    if(arr[i]>m)
-    {
-      arr[i+1] = arr[i];
-    }
-    else 
-    {
-      arr[i+1] = m;
-      break;
-    }
+    printf("%d",arr[i]);
   }
-  //要输入的元素小于所有的元素
-  if(i<0)
-  {
-    arr[0] = m;  
-  }
-  for(i=0;i<n+1;i++)
-  {
-    printf("%d ",arr[i]);
-  }
-  return 0;
+  printf("\n");
+  return 0 ;
 }
