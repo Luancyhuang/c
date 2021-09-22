9b7791f08447d995168cd01f224d37f68266d91f
Luancyhuang <542914825@qq.com>
2021-09-22 15:14:33 +0800

    数组按要求装置

--- test3.c
+++ test3.c
@@ -1,28 +1,81 @@
 #include <stdio.h>
-//空心三角形
+//矩阵转置
+//int main()
+//{
+//  int n = 0;
+//  int m = 0;
+//  int arr[10][10] = {0};
+//  scanf("%d %d",&n,&m);
+//  int i = 0;
+//  int j = 0;
+//  for(i=0;i<n;i++)
+//  {
+//    for(j=0;j<m;j++)
+//    {
+//      scanf("%d",&arr[i][j]);     
+//    }
+//  }
+//  for(i=0;i<m;i++)
+//  {
+//    for(j=0;j<n;j++)
+//    {
+//      printf("%d ",arr[j][i]);
+//    }
+//    printf("\n");
+//  }
+//  return 0;
+//}
+//kiki有一个矩阵，他想知道通过k次行变换或列变换后得到的矩阵，请编程帮他解答
+
 int main()
 {
   int n = 0;
-  while(~scanf("%d",&n))
+  int m = 0;
+  int arr[10][10] = {0};
+  scanf("%d %d",&n,&m);
+  int i = 0;
+  int j = 0;
+  for(i=0;i<n;i++)
   {
-    int i = 0;
-    for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
     {
-      int j = 0;
+      scanf("%d",&arr[i][j]);    
+    }
+  }
+  int k = 0;
+  char t = 0;
+  int a = 0;
+  int b = 0;
+  scanf("%d",&k);
+  for(i=0;i<k;i++)
+  {
+    scanf(" %c %d %d",&t,&a,&b);
+    if(t == 'r')
+    {
+      for(j=0;j<m;j++)
+      {
+        int tmp = arr[a-1][j];
+	arr[a-1][j] = arr[b-1][j];
+	arr[b-1][j] = tmp;
+      }
+    }
+    else if(t == 'c')
+    {
       for(j=0;j<n;j++)
       {
-        if(i==n-1||j==0||i==j)
-	{
-	  printf("* ");
-	}
-	else
-	{
-	  printf("  ");
-	}
+        int tmp = arr[j][a-1];
+	arr[j][a-1] = arr[j][b-1];
+	arr[j][b-1] = tmp;      
       }
-      printf("\n");    
     }
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<m;j++)
+    {
+      printf("%d ",arr[i][j]);
+    }
+    printf("\n");
   }
   return 0;
 }
-
