1834ceef35187ad82f60f2f6c7abcb79b2a7d50c
Luancyhuang <542914825@qq.com>
2021-09-27 16:58:02 +0800

    试除法求解三位数中质数的个数

--- test2.c
+++ test2.c
@@ -1,126 +1,35 @@
 #include <stdio.h>
-
+//BoBo老师教了KiKi学习程序设计基础，他知道C++是带类的C语言，这个“++”主要包含三部分内容：对C语言进行语法上的扩展、面向对象（封装、继承和多态），STL（即模板）。这学期KiKi学习了C和C++，学号他们，其他编程语言都可以很轻松地掌握。C和C++的输入有区别，请帮KiKi输出C和C++输出hello world的代码。 
 //int main()
 //{
-//  int n = 0;
-//  int m = 0;
-//  scanf("%d %d",&n,&m);
-//  int i = 0;
-//  int j = 0;
-//  int num = 0;
-//  for(i=1;i<=n*m;i++)
-//  {
-//    scanf("%d",&num);
-//    printf("%d ",num);
-//    if(i%m == 0)
-//    {
-//      printf("\n");
-//    }
-//  }
+//  printf("printf(\"hello,world!\\n\");\n");
+//  printf("cout << \"hello,world!\" << end1;\n");
 //  return 0;
 //}
-//从n行m列的方阵队列找到身高最高的人的位置，请你帮助他完成这个任务。
-//int main()
-//{
-//  int n = 0;
-//  int m = 0;
-//  scanf("%d %d",&n,&m);
-//  int i = 0;
-//  int j = 0;
-//  int num = 0;
-//  int max = 0;
-//  int x = 0;
-//  int y = 0;
-//  for(i=0;i<n;i++)
-//  {
-//    for(j=0;j<m;j++)
-//    {
-//      scanf("%d",&num);
-//      if(num>max)
-//      {
-//        max = num;
-//	x = i;
-//	y = j;
-//      }
-//    }  
-//  }
-//  printf("%d %d\n",x+1,y+1);
-//  return 0;
-//}
-//矩阵相等判断
-//int main()
-//{
-//  int n = 0;
-//  int m = 0;
-//  scanf("%d %d",&n,&m);
-//  int arr1[10][10] = {0};
-//  int arr2[10][10] = {0};
-//  int i = 0;
-//  int j = 0;
-//  for(i=0;i<n;i++)
-//  {
-//    for(j=0;j<m;j++)
-//    {
-//      scanf("%d",&arr1[i][j]);    
-//    }  
-//  }
-//  for(i=0;i<n;i++)
-//  {
-//    for(j=0;j<m;j++)
-//    {
-//      scanf("%d",&arr2[i][j]);    
-//    }  
-//  } 
-//  for(i=0;i<n;i++)
-//  {
-//    for(j=0;j<m;j++)
-//    {
-//      if(arr1[i][j] != arr2[i][j])
-//      {
-//        printf("No!\n");
-//	return 0;      
-//      }
-//    }
-//  }
-//  printf("Yes\n");
-//  return 0;
-//}
-//上三角矩阵的判断
-int main()
+//KiKi知道了什么事质数（只能被1和他自身整除的数），他现在想知道多有三位整数中，有多少个质数。
+int is_prime(int n)
 {
-  int n = 0;
-  scanf("%d",&n);
-  int arr[10][10] = {0};
-  int i = 0;
+  //拿2～n-1之间数整除
   int j = 0;
-  for(i=0;i<n;i++)
+  for(j=2;j<=n-1;j++)
   {
-    for(j=0;j<n;j++)
+    if(n%j==0)
     {
-      scanf("%d",&arr[i][j]);    
-    }  
-  }
-  for(i=0;i<n;i++)
-  {
-    for(j=0;j<n;j++)
-    {
-      if(i>j)
-      {
-        if(arr[i][j]!=0)
-	{
-	  printf("No\n");
-	  return 0;	
-	}      
-      }
+      return 0;
     }
   }
-  printf("Yes\n");  
+  return 1;
+}
+int main()
+{
+  int i = 0;
+  int count = 0;
+  for(i=100;i<=999;i++)
+  {
+    if(is_prime(i)==1)
+    count++;  
+  }
+  printf("%d\n",count);
   return 0;
 }
-
-
-
-
-
-
 
