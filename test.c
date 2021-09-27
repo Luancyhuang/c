16b09b371dbb450ef94f55eeca1374e969185e16
Luancyhuang <542914825@qq.com>
2021-09-27 11:05:25 +0800

    去重排序

--- test.c
+++ test.c
@@ -1,54 +1,23 @@
 #include <stdio.h>
-//辗转相除法求解两个数的最大公约数和最小公倍数。
-//int main()
-//{
-//  long long n1 = 0;
-//  long long m1 = 0;
-//  scanf("%lld %lld",&n1,&m1);
-//  long long n2 = n1;
-//  long long m2 = m1;
-//  long long max = 0;
-//  long long min = 0;
-//  long long tmp = 0;
-//  while(tmp=n1%m1)
-//  {
-//    n1=m1;
-//    m1=tmp;  
-//  }
-//  max = m1;
-//  min = n2*m2/max;
-//  printf("%lld\n",max+min);
-//  return 0;
-//}
-//小乐乐喜欢数字，尤其喜欢0和1。他现在得到了一个数，想把每位的数变成0和1如果某一位是奇数，就把他变成1，如果是偶数，那么就把它变成0.请你回答他最后得到的数是多少？
-#include <math.h>
+//老师给小乐乐一个正整数序列，要求小乐乐把这个序列去重后按从小到大排序。但是老师给出的序列太长了，小乐乐没办法耐心的去重并排序，请你帮助他。
 int main()
 {
   int n = 0;
+  int arr[10001]={0};
   scanf("%d",&n);
   int i = 0;
-  int sum = 0;
-  while(n)
+  int tmp = 0;
+  for(i=0;i<n;i++)
   {
-    int m = n%10;
-    if(m%2==1)
-    {
-      m=1;    
-    }
-    else 
-    {
-      m=0;
-    } 
-    sum += m*pow(10,i);
-    i++;
-    n/=10;
+    scanf("%d",&tmp);
+    arr[tmp] = tmp;   
   }
-  printf("%d\n",sum);
+  for(i=0;i<n;i++)
+  {
+    if(arr[i]!=0)
+    {
+      printf("%d ",arr[i]);    
+    }  
+  }
   return 0;
 }
-//
-//
-//
-//
-//
-//
