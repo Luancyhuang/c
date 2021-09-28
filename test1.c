89250aed2a7f8e19f4df6c4bcb063453353aab24
Luancyhuang <542914825@qq.com>
2021-09-28 22:53:34 +0800

    单链表

--- test1.c
+++ test1.c
@@ -1,93 +1,110 @@
 #include <stdio.h>
-//int main()
-//{
-//  int i = 0;
-//  int score[5] = {0};
-//  for(i=0;i<5;i++)
-//  {
-//    int j = 0;
-//    float sum = 0.0;
-//    for(j=0;j<5;j++)
-//    {
-//      scanf("%f",score[j]);
-//      sum += score[j];
-//    }
-//    for(j=0;j<5;j++)
-//    {    
-//      printf("%.1f ",score[j]);
-//    }
-//    printf("%.1f\n",sum);    
-//  }
-//  return 0 ;
-//}
-//
-//矩阵元素定位
+
 //int main()
 //{
 //  int n = 0;
-//  int m = 0;
-//  scanf("%d %d",&n,&m);
-//  //接受n行m列数组
-//  int arr[10][10] = {0};
+//  int arr[1001] = {0};//拿空间换取时间
+//  scanf("%d",&n);
+//  
 //  int i = 0;
-//  int j = 0;
+//  int m = 0;
+//  //循环两次
 //  for(i=0;i<n;i++)
 //  {
-//    for(j=0;j<m;j++)
+//    scanf("%d",&m);
+//    arr[m] = m;
+//  }
+//
+//  for(i=0;i<=1000;i++)
+//  {
+//    if(arr[i]!=0)
 //    {
-//      scanf("%d",&arr[i][j]);    
+//      printf("%d ",arr[i]);    
 //    }  
 //  }
-//  int x = 0;
-//  int y = 0;
-//  scanf("%d %d",&x,&y);
-//  printf("%d\n",arr[x-1][y-1]);
 //  return 0;
 //}
-//序列重组矩阵
+//
+//
+struct Node
+{
+  int data;
+  struct Node *next;
+}
 int main()
 {
+  struct Node *list = NULL;
+  struct Node *tail = NULL;
+
   int n = 0;
-  int m = 0;
-  int arr[10][10] = {0};
-  scanf("%d %d",&n,&m);
+  scanf("%d",&n);
+
   int i = 0;
-  int j = 0;
+  int m = 0;
+  int d = 0;//要删除的元素
   for(i=0;i<n;i++)
   {
-    for(j=0;j<m;j++)
+    scanf("%d",&m);
+    struct Node *pn = (struct Node*) malloc(sizeof(struct Node));
+    pn->data = m;
+    pn->next = NULL;
+    
+    if(list == NULL)
     {
-      scanf("%d",&arr[i][j]);
+      list = pn;
+      tail = pn;    
+    }
+    else
+    {
+      tail->next = pn;
+      tail = pn;    
+    }
+  }
+  scanf("%d",&d);//接收要删除的元素
+  //删除链表中指定的元素
+  struct Node *cur = list;
+  struct Node *prev = NULL;
+  while(cur)
+  {
+    if(cur->data == d)
+    {
+      //删除
+      //删除第一个节点
+      struct Node *pd = cur;
+      if(cur == list)
+      {
+        list = list->next;
+	cur = list;      
+      }
+      else
+      {
+        prev->next = cur->next;
+	cur = prev->next;
+      }
+      free(pd);
+      n--;
+    }
+    else 
+    {
+      cur = cur->next;
     }  
   }
-  for(i<0;i<n;i++)
+  printf("%d\n",n);
+  cur = list;
+  while(cur)
   {
-    for(j=0;j<m;j++)
-    {
-      printf("%d ",arr[i][j]);
-    }
-    printf("\n");
+    printf("%d ",cur->data);
+    cur = cur->next;  
   }
+  //释放单链表
+  cur = list;
+  struct Node *del = NULL;
+  while(cur)
+  {
+    del = cur;
+    cur = cur->next;
+    free(del);   
+  }
+  list = NULL;
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
